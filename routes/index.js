fa4cf1c6d3301189dc7bf74e95f0bc45667325c2
DANNY DALEY <dd252935@falmouth.ac.uk>
2021-09-30 16:40:32 +0100

    Added logic to login page and some login validation, leading to the 'Loggedin' page if form validates

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 let express = require('express');
 let router = express.Router();  
 
-/* GET home page. */
+
+
+
+
+
+function validateFormData(data) { 
+  let loggedIn = false;
+  //is username and password correct?
+  if (data.username){
+    loggedIn = true;
+    console.log("VALIDDDDDDD");    
+  }  else {
+    console.log("noooope")
+    loggedIn = false;
+  }
+    return loggedIn;
+}
+
+
+
+  /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -9,6 +29,25 @@ router.get('/', function(req, res, next) {
 /* GET login page. */
 router.get('/login/', function(req, res, next) {
   res.render('login', { title: 'Log in' });
+});
+
+router.get('/loggedIn/', function(req, res, next) {
+  res.render('loggedIn', { title: 'logged in '});
+});
+
+router.post('/login/', function(req, res, next) {
+  let loginValid = validateFormData(req.body);
+  if (loginValid){
+    console.log("YEEEEEP");
+    res.render('loggedIn', { username: req.body.username });
+  } else {
+    res.render('/login/', { name: undefined, password: undefined });
+    console.log("NOOOOPE");
+  }
+});
+
+router.get('/name/', function(req, res, next) {
+  res.render('name', { name: req.query.name });
 });
 
 module.exports = router;
