de40c21a5ddde6c88f6d2baa93034c8b5f2c1f8d
DANNY DALEY <dd252935@falmouth.ac.uk>
2021-10-01 19:05:17 +0100

    added further styling to blog.hbs, moved userDatabase into its own JSON file and added further content to posts.JSON

--- public/javascripts/postPreviews.js
+++ public/javascripts/postPreviews.js
@@ -1,6 +1,23 @@
+// import {postData} from '../../routes/index';
+
+
+// let posts = postData;
+
+// let projects = () =>  posts.map(project =>
+//     document.getElementById('projects').innerHTML += 
+//     `<div class="project-tile">
+//         <a href="" target="_blank">
+//             <img alt="Project Thumbnail" src="${project.image}"/>
+//             <p>"${project.title}"</p>
+//         </a>  
+//     </div>`
+// );
+
+
+// projects();
 
 
 
-console.log("HHHEEEEEELLLOOOOOOOOOOO");
+// console.log("HHHEEEEEELLLOOOOOOOOOOO");
 
-alert("HHEEELLOOOOOOO");
+// alert("HHEEELLOOOOOOO");
--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,8 @@
 let express = require('express');
 let postData = require("../posts.json");
+let userDatabase = require("../userDatabase.json");
 
-
-
-let router = express.Router();  
-
-
-let database = {
-  users: [
-    {
-      id: '123',
-      name: 'Danny',
-      email: 'dannydaley@outlook.com',
-      password: 'password',
-      posts: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'DannyD1990',
-      email: 'dandaley1990@hotmail.com',
-      password: 'password123',
-      posts: 0,
-      joined: new Date()
-    }
-  ]
-}
-
-
-
-
-// function validateFormData(data) { 
-//   let loggedIn = false;
-//   //is username and password correct?
-//   if (data.username === 'Danny' && data.password === 'password'){
-//     loggedIn = true;
-//     console.log("VALIDDDDDDD");    
-//   }  else {
-//     console.log("noooope")
-//     loggedIn = false;
-//   }
-//     return loggedIn;
-// }
-
-
+let router = express.Router(); 
 
   /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -51,26 +10,29 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET login page. */
-router.get('/login/', function(req, res, next) {
+router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Log in' });
 });
 
-router.get('/loggedIn/', function(req, res, next) {
+
+router.get('/loggedIn', function(req, res, next) {
   res.render('loggedIn', { title: 'logged in '});
 });
 
+/* GET home page. */
+router.get('/blog', function(req, res, next) {
+  res.render('blog', postData);
+});
 
-  /* GET home page. */
-  router.get('/blog', function(req, res, next) {
-    res.render('blog', postData);
-  });
-
-
+router.get('/name', function(req, res, next) {
+  res.render('name', { name: req.query.name });
+});
 ////////////////////////////////////////////
-router.post('/login/', function(req, res, next){
+//login validation
+router.post('/login', function(req, res, next){
   let found = false;
-  for (let i = 0; i < database.users.length; i++) {
-    if (req.body.email === database.users[i].email && req.body.password === database.users[i].password) {      
+  for (let i = 0; i < userDatabase.users.length; i++) {
+    if (req.body.email === userDatabase.users[i].email && req.body.password === userDatabase.users[i].password) {      
       found = true;
     } 
   }
@@ -81,22 +43,21 @@ router.post('/login/', function(req, res, next){
   };
 })
 
+//adds new user to user database
 router.post('/register', function (req, res, next) {
   let { email, name, password } = req.body;
-  database.users.push({
-    id: database.users.length,
+  userDatabase.users.push({
+    id: userDatabase.users.length,
     name: name,
     email: email,
     password: password,
     posts: 0,
     joined: new Date()
   })
-  res.json(database.users[database.users.length-1]);
+  res.json(userDatabase.users[userDatabase.users.length-1]);
 })
 
 
-router.get('/name/', function(req, res, next) {
-  res.render('name', { name: req.query.name });
-});
+
 
 module.exports = router;
